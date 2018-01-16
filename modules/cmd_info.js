c94780b3dd15f77dd5bfea427603b648ad32e3d3
oblaqoo <admin@oblaqoo.ru>
2018-01-16 22:36:18 +0500

    new cmd: tinfo

--- modules/cmd_info.js
+++ modules/cmd_info.js
@@ -1,7 +1,37 @@
+var request = require('request');
+var fs = require("fs");
 module.exports = {
 	msg:{
 		'info':{
 			aliases: ["info","инфа"],
+			go:function(cbot,vk,msg,body,tbody,obody){ //cbot = CloudBOT interface; vk = vk promise interface; msg = msg object; body = тело сообщения; tbody = вызванный aliase команды; cbody = тело сообщения без aliase
+				var ASC = cbot.service.ASC[msg.chat_id];
+				var BSC = cbot.service.BSC[msg.chat_id];
+				msg.get().then(function(data){
+					var fname = cbot.config.tmp+"/"+Date.now()+"_"+msg.user_id+".jpg";
+					var w = fs.createWriteStream(fname);
+					request("https://api.oblaqoo.ru/bot.pctr?type=chat_info&data="+encodeURIComponent(JSON.stringify({
+						type: (msg.chat_id?1:0),
+						did: (msg.chat_id?msg.chat_id:msg.user_id),
+						name: (msg.chat_id?msg.title:'Здесь должно быть Ваше имя)'),
+						admin_id: (msg.chat_id?data.admin_id:0),
+						mm: (data.admin_id==cbot.config.bot_id?(BSC?1:0):2),
+						antimat: (BSC?BSC.antimat:0),
+						max_warns: (BSC?BSC.max_warns:0),
+						freemode: (ASC?ASC.freemode:0),
+						open: (ASC?ASC.open:0),
+						freevoice: (ASC?ASC.voice:0),
+						chat_admin: (BSC?BSC.admin:0),
+					}))).pipe(w);
+					w.on('finish', function(){
+						console.log('file downloaded ', fname);
+						msg.sendPhoto(fs.createReadStream(fname));
+					});
+				});
+			},
+		},
+		'tinfo':{
+			aliases: ["textinfo","tinfo","тинфа"],
 			go:function(cbot,vk,msg,body,tbody,obody){ //cbot = CloudBOT interface; vk = vk promise interface; msg = msg object; body = тело сообщения; tbody = вызванный aliase команды; cbody = тело сообщения без aliase
 				var ASC = cbot.service.ASC[msg.chat_id];
 				var BSC = cbot.service.BSC[msg.chat_id];
